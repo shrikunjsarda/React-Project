c3d85a644d7c14691f3ec6ab2d94cdda466325ca
oyo <oyo@Administrators-MacBook-Pro.local>
2021-02-18 11:52:49 +0530

    Adding project

---
+++ src/Components/ButtonRegister/RegisterButtonStyle.ts
@@ -0,0 +1,17 @@
+// import React from 'react';
+import styled from 'styled-components';
+
+const RegisterButtonStyle = styled.button`
+  display: block;
+  background-color: #0089fe;
+  color: #fff;
+  font-size: .9rem;
+  border-radius: 5px;
+  margin: auto;
+  height: 40px;
+  padding: 0px 20px;
+  cursor: pointer;
+  box-sizing: border-border-box;
+`;
+
+export default RegisterButtonStyle;
---
+++ src/Components/InputArea/InputArea.ts
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+// import sharedStyles from '../sharedStyle';
+
+
+const InputArea = styled.input`
+    display: block;
+    width: 100%;
+    background-color: #eeeeee;
+    height: 40px;
+    border-radius: 5px;
+    border: 1px solid #dddddd;
+    margin: 10px 0 20px 0;
+    padding: 20px;
+    box-sizing: border-box;
+`;
+
+export default InputArea;
